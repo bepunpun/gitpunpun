42a119d999d645c9caa88d10f20f1545c7dedf4a
punpun <dridibilel505@gmail.com>
2023-06-15 01:29:26 +0100

    punpun file

---
+++ pun.c
@@ -0,0 +1,5 @@
+hello world this my first project for my git formation to github so it just a simple tewt in nano i wanna use vim for real but not now any why
+#include <stdio.>
+int main(){
+printf("hello world")
+}
